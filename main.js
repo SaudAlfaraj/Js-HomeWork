8aa294b8e9c0d00d1acd974fa7dd5c77ac29973b
Saud Alfaraj <saud.alfaraj15@icloud.com>
2023-08-07 18:56:52 +0300

    Done

---
+++ main.js
@@ -0,0 +1,37 @@
+// Saud Saad Alfaraj
+
+// age
+let age = 14;
+age = 15;
+// name
+let name = 'Saud Saad';
+name = "alfaraj";
+
+console.log(age);
+console.log(name);
+
+
+// 2
+
+// const hi = 'hi'
+// hi = 'Hi'
+
+// console.log(hi);
+
+// 3
+
+{
+    var s = 'H'
+}
+
+// {
+//     let Sa = "Sa";
+// }
+
+// {
+//     const la = "la"
+// }
+
+console.log(s);
+// console.log(Sa);
+// console.log(la);
