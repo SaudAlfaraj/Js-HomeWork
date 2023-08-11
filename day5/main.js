1399bfd1f021cbe5449fa09ff830566ba63c565e
Saud Alfaraj <saud.alfaraj15@icloud.com>
2023-08-11 03:57:33 +0300

    Day5

---
+++ day5/main.js
@@ -0,0 +1,79 @@
+// const space = "----------------";
+// // أول ماتعلمناه هو المتغيرات والثوابت
+
+// let str = "STR";
+// // أنشئنا متغير نصي
+// console.log(str);
+// // نطبع المتغير
+// const str2 = "STR";
+// // أنشئنا ثابت نصي
+// console.log(str2);
+// // نطبع الثابت
+// let num = 14;
+// // هنا أنشئنا متغير رقم
+// console.log(num);
+// // نطبع المتغير
+
+// const num2 = 14;
+// // أنشئنا ثابت رقم
+// console.log(num2);
+// // نطبع الثابت
+
+// // عمليات حسابية بسيطة
+// // نقوم بجمع وطرح النصوص ونترقب النتيجة
+// let strmath = str + str2;
+// console.log(strmath);
+// // النتيجة هنا تكون STRSTR
+
+// // عمليات حسابية بين رقمين
+// let nummath = num + num2;
+// console.log(nummath);
+// // النتيجة راح تكون28 
+
+// // أنواع البيانات الأخرى
+// let bol = true;
+// let dub = 10.1;
+// let und = undefined;
+// let nul = null;
+// let ob1 = Object();
+// let sy = Symbol();
+// console.log(bol);
+// console.log(und);
+// console.log(nul);
+// console.log(ob1);
+// console.log(sy);
+// console.log(typeof dub);
+// console.log(typeof bign);
+// console.log(space);
+
+// // معرفة أنواع البيانات
+
+// // فلنتعرف كيف ان نعرف صيغة المتغير أو الثابت
+
+// console.log(typeof str);
+// // النتيجة ستكون String
+
+// // فلنجرب معرفة معرفة المتغير الرقمي
+
+// console.log(typeof num);
+
+// // مرحلة السكوب والأوبجكت
+
+// {
+//     let hi = "HI"
+//     console.log(hi);
+// }
+
+// let ob = {
+//     name: 'Saud',
+//     age:14
+// }
+// console.log(ob);
+
+// // مرحلة الإقتصاص والعد
+// let spil = "Java Script";
+// console.log(spil.charAt(5)); // النتيجة هي S
+// console.log(spil.length); // 11
+// console.log(spil.slice(4)); //راح يطلع لي جملة سكربت
+// console.log(spil.split(" "));
+
