f7626168dea049f1e8ca14f6e19f6c26ff7201e2
Saud Alfaraj <saud.alfaraj15@icloud.com>
2023-08-14 01:33:47 +0300

    Day6

---
+++ day6/main.js
@@ -0,0 +1,26 @@
+const space = "------------------------";
+// ----------------------------------------- //
+
+let names = ["Ali","Samah","Kahald","Fahad","Mohammad"];
+
+console.log(names[2]);
+
+console.log(space);
+
+console.log(names.unshift("Saud"));
+console.log(names);
+
+console.log(space);
+
+console.log(names.push("Ibraham"));
+console.log(names);
+
+console.log(space);
+
+console.log(names.shift());
+console.log(names);
+
+console.log(space);
+
+console.log(names.pop());
+console.log(names);
