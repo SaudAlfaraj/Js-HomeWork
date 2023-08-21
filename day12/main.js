f46097ec90ac43ad3d0ce150cdf27d82f1f2b34c
Saud Alfaraj <saud.alfaraj15@icloud.com>
2023-08-21 14:16:05 +0300

    homework

---
+++ day12/main.js
@@ -0,0 +1,15 @@
+function javascript_Boot_camp(techer1 , techer2) {
+    alert(" This javaScript BootCamp with "+ techer1 + "Ok ?"  );
+    alert(" This javaScript BootCamp with "+ techer2 + "Ok ?"  );
+
+}
+
+javascript_Boot_camp("Techer1 " ,"Techer2 ")
+
+function numbers(num1 , num2) {
+    let math = (num1 + num2) /2;
+    console.log(math);
+}
+
+numbers(5 , 3)
+numbers(5.3,3.45)
